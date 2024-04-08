c1f06968a856677754c9c11939a97c92aafe6f9d
juampi74 <juampijaca@gmail.com>
2024-04-08 20:57:51 -0300

    Agregué un IF en el main.js y un h3 en el index.html

--- main.js
+++ main.js
@@ -1,2 +1,14 @@
 console.log("Hola Mundo!");
 condole.log("Saliooo!");
+
+const deporteNahu = "Tenis";
+
+if (deporteNahu == "Futbol") {
+  console.log("Banqué!");
+} else if (deporteNahu == "Tenis") {
+  console.log(
+    "Tiene que pasar por el comité de chequeo (99 días hábiles de espera)"
+  );
+} else {
+  console.log("Se cae");
+}
